1ccbae6d4e5d4f3b0495ffad2d6c9a0d165cf53c
Sylwia <sylwia_24_24@wp.pl>
2019-07-13 19:25:18 +0200

    Add class active, select href

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.getElementById('test-button').addEventListener('click', function(){
   console.log('links:', links);
 });
 
+event.preventDefault();
 const titleClickHandler = function(event){
 	const clickedElement = this;
   	console.log('Link was clicked!');
@@ -16,6 +17,7 @@ for(let activeLink of activeLinks){
   /* [IN PROGRESS] add class 'active' to the clicked link */
 console.log('clickedElement:', clickedElement);
 clickedElement.add('active');
+
   /* remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.article a.active');
 
@@ -24,14 +26,20 @@ for(let activeArticle of activeArticles){
 }
 
   /* get 'href' attribute from the clicked link */
+const articleSelector = document.querySelectorAll('.href .href');
+clickedElement.getAttribute('href')
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+const targetArticle = document.querySelector('href')
   /* add class 'active' to the correct article */
+console.log('targetArticle:', targetArticle);
+clickedElement.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+
 }
