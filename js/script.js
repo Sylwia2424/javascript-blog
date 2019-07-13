e18ab04e157737ed9113e91e2a2f31ae7136f758
Sylwia <sylwia_24_24@wp.pl>
2019-07-13 18:41:01 +0200

    Add class active

--- js/script.js
+++ js/script.js
@@ -4,16 +4,18 @@ document.getElementById('test-button').addEventListener('click', function(){
 });
 
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
+	const clickedElement = this;
+  	console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /*[DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
-  /* add class 'active' to the clicked link */
-
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+console.log('clickedElement:', clickedElement);
+clickedElement.add('active');
   /* remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.article a.active');
 
